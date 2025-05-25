cff9e0a35922c9d4e08a988ddadd5953a0e6df3b
Weiqi ZHANG <weiqi.zhang_arthur@yahoo.com>
2025-05-25 19:44:44 +0200

    fix: format

--- eval/answerer.py
+++ eval/answerer.py
@@ -8,6 +8,6 @@ class Answerer(QAChain):
         new_qa = QA(question=qa.question,
                     context=qa.context,
                     expected_output=qa.expected_output,
-                    actual_output = response.answer,
+                    actual_output=response.answer,
                     retrieval_context=[document["page_content"] for document in response.retrieved_documents])
         return new_qa
