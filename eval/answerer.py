a85cc00663af0a5b0f13fbb30ac51b21acd49ad7
Weiqi ZHANG <weiqi.zhang_arthur@yahoo.com>
2025-05-25 19:44:25 +0200

    fix: Answerer class makes qa.retrieval_context into list[str] instead of list[dict]

--- eval/answerer.py
+++ eval/answerer.py
@@ -1,27 +1,13 @@
-from langchain_ollama import OllamaLLM
-import torch
-
-from rag.vectorstore import prepare_vectorstore
 from rag.qa_chain import QAChain
 from qa import QA
 
 
-class Answerer:
-    def __init__(self, llm_name="tinyllama:1.1b",
-                 vectorstore=prepare_vectorstore(embedding_model_name="sentence-transformers/all-MiniLM-L6-v2",
-                                                 document_directory="../data/corpus",
-                                                 device='cuda' if torch.cuda.is_available() else 'cpu')):
-        self.llm = OllamaLLM(
-            model=llm_name,
-            temperature=0.7,
-            top_p=0.95,
-            repeat_penalty=1.15,
-            verbose=True
-        )
-        self.vectorstore = vectorstore
-        self.qa_chain = QAChain(self.vectorstore, self.llm)
-
+class Answerer(QAChain):
     def answer(self, qa: QA) -> QA:
         response = self.qa_chain.qa_with_sources(query=qa.question)
-        qa.actual_output = response.answer
-        return qa
+        new_qa = QA(question=qa.question,
+                    context=qa.context,
+                    expected_output=qa.expected_output,
+                    actual_output = response.answer,
+                    retrieval_context=[document["page_content"] for document in response.retrieved_documents])
+        return new_qa
